e390e35bc0b01cdc01854e0967f668ae00a7c054
Gustavo Henrique <gustavohenri316@icloud.com>
2021-09-24 11:36:17 -0300

    logog

--- src/pages/Home/componets/Header.js
+++ src/pages/Home/componets/Header.js
@@ -3,11 +3,18 @@ import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-
+import SvgIcon from '@material-ui/core/SvgIcon';
+import { Bell } from 'react-feather';
 
 const useStyles = makeStyles ({
     appBar:{
         boxShadow: 'none',
+    },
+    img: {
+        maxHeight:'55',
+    },
+    grow: {
+        flexGrow: 1
     }
 });
 
@@ -17,6 +24,14 @@ function Header() {
     return (
         <AppBar position="fixed" color="inherit" className={classes.appBar}>
         <Toolbar>
+            <img src="/images/logog.png" alt="logo" className={classes.img}/>
+            <div className={classes.grow}></div>
+            <div>
+            <Button color='primary' variant='contained'>Novo Post</Button>
+            <SvgIcon>
+                <Bell></Bell>
+            </SvgIcon>
+            </div>
            {/*<div>
                 <span>GuhCode</span>
             </div>
